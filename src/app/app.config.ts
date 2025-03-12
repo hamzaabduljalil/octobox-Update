058c8ada5ac4e679e47734bdd2006abdd217c099
Mhd.R Nasser <mhd.rabee.naser@gmail.com>
2025-03-12 09:58:55 +0300

    up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import {
@@ -31,7 +35,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // ɵprovideZonelessChangeDetection(),
+    provideZoneChangeDetection(),
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
     provideRouter(routes, withHashLocation(), withViewTransitions()),
     provideAnimations(),
--- src/app/services/other/change-lang.service.ts
+++ src/app/services/other/change-lang.service.ts
@@ -1,35 +1,51 @@
-import { Injectable, computed, signal } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Inject,
+  Injectable,
+  PLATFORM_ID,
+  computed,
+  signal,
+} from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class ChangeLangService {
-  position = signal("left");
+  private lang: string | '' = '';
+  private isBrowser = false;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    private translateSerivce: TranslateService
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  position = signal('left');
   currentLang = signal(
-    localStorage.getItem("lang") ? localStorage.getItem("lang") : "en"
+    this.isBrowser && localStorage.getItem('lang')
+      ? localStorage.getItem('lang')
+      : 'en'
   );
   currentDirection = computed(() => {
-    if (this.currentLang() === "en") {
+    if (this.currentLang() === 'en') {
       this.translateSerivce.use(this.currentLang()!);
-      return "ltr";
-    } else if (this.currentLang() === "ar") {
+      return 'ltr';
+    } else if (this.currentLang() === 'ar') {
       this.translateSerivce.use(this.currentLang()!);
-      return "rtl";
-    } else return "";
+      return 'rtl';
+    } else return '';
   });
 
-  constructor(private translateSerivce: TranslateService) {}
-
   changeLang(langCode: string): void {
-    localStorage.setItem("langCode", langCode.toString());
-    if (langCode === "ar") {
-      this.currentLang.set("ar");
-      this.position.set("right");
+    if (this.isBrowser) {
+      localStorage.setItem('langCode', langCode.toString());
     }
-    if (langCode === "en") {
-      this.currentLang.set("en");
-      this.position.set("left");
+    if (langCode === 'ar') {
+      this.currentLang.set('ar');
+    } else {
+      this.currentLang.set('en');
     }
   }
 }
